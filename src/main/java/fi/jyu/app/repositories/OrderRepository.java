a8502dde72872e0c2cf8b503d430bff459c2f31d
Tuukjazz <158285359+Tuukjazz@users.noreply.github.com>
2024-02-06 23:09:33 +0200

    Update and rename OfferRepository.java to OrderRepository.java

--- src/main/java/fi/jyu/app/repositories/OrderRepository.java
+++ src/main/java/fi/jyu/app/repositories/OrderRepository.java
@@ -3,9 +3,9 @@ package fi.jyu.app.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import fi.jyu.app.models.Offer;
+import fi.jyu.app.models.Order;
 
 @Repository
-public interface OfferRepository extends JpaRepository<Offer, Integer> {
+public interface OrderRepository extends JpaRepository<Order, Integer> {
     
 }
