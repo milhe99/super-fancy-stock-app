a0215e14f72185faa3ccea86c7f6e874185c5772
Milla Henriksson <mirehenr@jyu.fi>
2024-02-09 21:21:57 +0200

    Add tests for docker

--- src/test/java/fi/jyu/superfancystockapp/DatacaseConnectionTest.java
+++ src/test/java/fi/jyu/superfancystockapp/DatacaseConnectionTest.java
@@ -1,4 +1,4 @@
-package fi.jyu.app;
+package fi.jyu.superfancystockapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
